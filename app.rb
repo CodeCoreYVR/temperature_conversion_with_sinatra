1e872a34593a1e3927d3690db444fac58ab03543
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-05-31 22:05:25 -0700

    make the result show on the same page
    We improve our solution now by making the result show on the same page. So instead of simply rendering the result as a text we just re-render the index ERB template: `erb :index`.
    
    In order to access the values in the ERB template we will need to make them instance variables so we switch the `temperature_in_c` to `@temperature_in_c` and the `temperature_in_f` to `@temperature_in_f`.
    
    We then use the ERB blocks to dispaly the results in Fahrenheit if it exists. The reason why we need to do so is because we use the same file `index.erb` for both actions: `get "/"` and `post "/convert"` and the `@temperature_in_f` doesn't exist in the first action.
    
    We also display the value in the input field by simply setting the `value` attribute on the HTML tag. For the `get "/"` action, the `@temperature_in_f` will be `nil` so it will just show nothing.

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get "/" do
 end
 
 post "/convert" do
-  temperature_in_c = params[:temperature].to_f
-  temperature_in_f = temperature_in_c * 9 / 5 + 32
-  temperature_in_f.to_s
+  @temperature_in_c = params[:temperature].to_f
+  @temperature_in_f = @temperature_in_c * 9 / 5 + 32
+  erb :index
 end
